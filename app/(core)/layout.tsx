9e9396b6a77e765aff4913ad60f970d33222cb1d
Mehdi <bhirimehdi28@gmail.com>
2025-08-19 12:24:45 +0100

    d

--- app/(core)/layout.tsx
+++
@@ -1,8 +0,0 @@
-"use client" // Added missing semicolon after use client directive
-
-import type React from "react"
-import { AppLayout } from "@/components/layout/app-layout"
-
-export default function CoreLayout({ children }: { children: React.ReactNode }) {
-  return <AppLayout>{children}</AppLayout>
-}
