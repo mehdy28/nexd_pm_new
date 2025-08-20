7d688266998cbd2cc5c3b46598a108ab100846d3
Mehdi <bhirimehdi28@gmail.com>
2025-08-20 15:54:39 +0100

    ddd

--- app/api/graphql/route.ts
+++
@@ -1,9 +0,0 @@
-import apolloServer from "@/lib/apollo-server"
-
-export async function GET(request: Request) {
-  return apolloServer(request)
-}
-
-export async function POST(request: Request) {
-  return apolloServer(request)
-}
