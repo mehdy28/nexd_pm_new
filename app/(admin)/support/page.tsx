a62188828ec353e15f2301279320dfb3cdae621c
Mehdi <bhirimehdi28@gmail.com>
2025-08-20 15:39:58 +0100

    d

--- app/(admin)/support/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import { CustomerSupport } from "@/components/admin/customer-support"
-
-export default function CustomerSupportPage() {
-  return <CustomerSupport />
-}
--- app/(admin)/users/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import { UserManagement } from "@/components/admin/user-management"
-
-export default function AdminUsersPage() {
-  return <UserManagement />
-}
