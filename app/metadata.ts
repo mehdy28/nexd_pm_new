8031274856a311a033149527093ad6ff0201fc3c
Mehdi <bhirimehdi28@gmail.com>
2025-12-25 17:01:31 +0100

    fhfh

--- app/metadata.ts
+++ app/metadata.ts
@@ -2,7 +2,10 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "NEXD.PM - Project Management",
-  description: "Modern project management platform",
+  title: "nexdpm",
+  description: "The First Ai-Powered Project Management Solution",
   generator: "v0.dev",
+  icons: {
+    icon: '/image-48x48 (1).jpg',
+  },
 };
