c029623c27ea0267d3704ff535d6b0fb6559a853
Mehdi <bhirimehdi28@gmail.com>
2026-01-14 19:11:27 +0100

    ddqd

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,9 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
 
-const prismaClientSingleton = () => {
-  return new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  })
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
