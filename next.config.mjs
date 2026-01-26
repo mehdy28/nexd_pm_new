aa54fd4f8775f90a864c7b0c778a288c7dcca6a8
Mehdi <bhirimehdi28@gmail.com>
2026-01-26 04:32:36 +0100

    hkjhk

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,22 @@ const nextConfig = {
   },
   reactStrictMode: false,
 
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.nexdpm.com',
+          },
+        ],
+        destination: 'https://nexdpm.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+
   async headers() {
     return [
       {
